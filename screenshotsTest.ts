2ee39872601e8016aa936cedde2f0f93287f877c
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2026-04-01 18:31:26 +0300

    Refactor `screenshotsTest.ts`

--- screenshotsTest.ts
+++ screenshotsTest.ts
@@ -10,29 +10,39 @@ if (!executablePath) {
     throw new Error('App Executable path not found. Please compile the app first using: npm run compile');
 }
 
-const electronApp = await electron.launch({
-    executablePath,
-    args: ['--no-sandbox'],
-});
+async function launchAndTakeScreenshot() {
+    const electronApp = await electron.launch({
+        executablePath,
+        args: ['--no-sandbox'],
+    });
 
-electronApp.on('console', (msg) => {
-    if (msg.type() === 'error') {
-        console.error(`[electron][${msg.type()}] ${msg.text()}`);
+    electronApp.on('console', (msg) => {
+        if (msg.type() === 'error') {
+            console.error(`[electron][${msg.type()}] ${msg.text()}`);
+        }
+    });
+
+    try {
+        const page = await electronApp.firstWindow();
+
+        // Capture errors
+        page.on('pageerror', (error) => console.error('Page error:', error));
+
+        // // Wait for the page to load
+        await page.waitForLoadState('load');
+        await page.waitForTimeout(1000);
+
+        await page.screenshot({ path: `screenshot.png`, fullPage: true });
+        await page.close();
+    } finally {
+        await electronApp.close();
     }
-});
-
-try {
-    const page = await electronApp.firstWindow();
-
-    // Capture errors
-    page.on('pageerror', (error) => console.error('Page error:', error));
-
-    // // Wait for the page to load
-    await page.waitForLoadState('load');
-    await page.waitForTimeout(1000);
-
-    await page.screenshot({ path: `screenshot.png`, fullPage: true });
-    await page.close();
-} finally {
-    await electronApp.close();
 }
+
+launchAndTakeScreenshot()
+    .then(() => {
+        console.log('Screenshot taken successfully');
+    })
+    .catch((error) => {
+        console.error('Error taking screenshot:', error);
+    });
